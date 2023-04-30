e461ecad4a737a2518efec8d32e1ba6dc2ca5a5a
kamrul-hasan01 <k17h02@gmail.com>
2023-04-30 13:30:12 +0600

    acart add "

--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -7,6 +7,8 @@ import {
 } from "../../SVGIcons";
 
 const Header = ({ cart }) => {
+  console.log("cart :", cart);
+  const total = cart?.reduce((pre, next) => pre + next.price * next.qty, 0);
   return (
     <header
       className="min-h-[65px] flex items-center relative z-[100]"
@@ -42,12 +44,48 @@ const Header = ({ cart }) => {
                   3
                 </span>
               </span>
-              <span className="w-[40px] h-[40px] flex justify-center items-center bg-[#34353A] rounded-xl relative">
+              <span className="w-[40px] h-[40px] flex justify-center items-center bg-[#34353A] rounded-xl relative group">
                 {" "}
                 <ShoppingCardIcon />
                 <span className="w-5 h-5 rounded-[50%] bg-[#DF4949] absolute -top-2 text-white -right-2 flex justify-center items-center">
                   {cart?.length}
                 </span>
+                <div className="absolute bg-white min-w-[250px] rounded-lg top-10 -right-12 hidden group-hover:block">
+                  <ul className="max-h-[400px] overflow-y-auto">
+                    {cart.map((item, index) => (
+                      <li
+                        key={index}
+                        className="flex justify-between items-center p-2"
+                      >
+                        <div className="w-[50px] h-[50px]">
+                          <img
+                            src={"/Avatar/" + item?.img_url}
+                            alt=""
+                            className="w-[60px] h-[60px] object-cover rounded-[5px]"
+                          />
+                        </div>
+                        <div className="flex flex-col justify-center items-start">
+                          <h1 className="text-[#34353A] text-[14px] font-medium">
+                            {item?.title.slice(0, 10)}...
+                          </h1>
+                          <span className="text-[#34353A] text-[12px] font-medium">
+                            ${item?.price}
+                          </span>
+                        </div>
+                        <div className="w-[20px] h-[20px] flex justify-center items-center bg-[#DF4949] rounded-[50%]">
+                          <span className="text-white text-[12px] font-medium">
+                            x{item?.qty}
+                          </span>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <button className="text-center bg-[green] w-10/12 block mx-auto text-white py-3 rounded my-2">
+                    Checkout{" "}
+                    <span className="font-extrabold pl-3">(${total})</span>
+                  </button>
+                </div>
               </span>
               <span className="w-[40px] h-[40px] flex justify-center items-center bg-[white] rounded-xl">
                 {" "}
